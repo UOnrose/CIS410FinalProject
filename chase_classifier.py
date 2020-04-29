cf80554c39860fc9b7023e4bd8783f675ffaa42d
Chase Craig <cmctheory@hotmail.com>
2020-04-29 15:05:24 -0700

    Added classifier class

---
+++ chase_classifier.py
@@ -0,0 +1,6 @@
+class Chase_Classifier():
+	def test(images):
+		pass
+	def train(images, labels):
+		pass
+	
---
+++ noah_classifier.py
@@ -0,0 +1,8 @@
+from chase_classifier import Chase_Classifier
+
+class Noah_Classifier(Chase_Classifier):
+	def test(images):
+		pass
+	def train(images, labels):
+		pass
+	
