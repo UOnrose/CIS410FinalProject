b6f309e5e4f2ff4b694256fdcb3d9e968e1eef7e
Noah Rose <nrose@uoregon.edu>
2020-05-06 17:45:42 -0700

    Main now uses our dataset, the dataset needs restructured though

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import os
 from noah_classifier import Noah_Classifier
 from chase_classifier import Chase_Classifier
 
+
 import PIL
 from PIL import Image
 def sliceImage(image, bbox,targSize=(128,128)):
@@ -20,7 +21,8 @@ def sliceImage(image, bbox,targSize=(128,128)):
 	# Returns a tensor
 	(w,h) = Img.size
 	return transforms.ToTensor()(transforms.functional.resized_crop(image, bbox[1]*h,bbox[0]*w, h*(bbox[3]-bbox[1]), w*(bbox[2] - bbox[0]), targSize))
-	
+
+
 def main():
 	parser = argparse.ArgumentParser(description='Loads the classifiers and either trains them or tests them on an image.')
 
@@ -58,16 +60,13 @@ def main():
 
 	# Fetch dataset 
 	# TODO use kaggle dataset rather than CIFAR-10
-	classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
+	classes = (0, 2, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 20, 21, 24, 25, 26, 31, 32, 37, 38, 40, 41, 44, 50, 51, 62, 67, 70, 71, 72, 73, 74, 75, 77, 78, 79, 80, 83, 86, 89, 90, 91, 92, 94, 96, 97, 98, 99, 100, 101, 102, 103, 104, 106, 108, 109, 110, 111, 112, 113, 114, 115, 116, 118, 119, 120, 121, 122, 123, 124, 127, 129, 130, 133, 134, 137, 139, 141, 142, 144, 145, 147, 150, 152, 153, 154, 156, 159, 161, 162, 163, 166, 167, 170, 175, 177, 179, 188, 198, 210, 221, 227, 229, 230, 233, 234, 235, 240, 242, 243, 245, 250, 251, 252, 253, 256, 257, 258, 259, 262, 265, 267, 268, 273, 279, 286, 290, 291, 292, 294, 296, 299, 300, 301, 302, 306, 307, 309, 310, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 352, 353, 354, 355, 356, 357, 370, 371, 372, 374, 375, 376, 377, 378, 379, 380, 382, 384, 385, 389, 390, 391, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 422, 454, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 599, 647, 648, 649, 650, 651, 652, 653, 654, 655, 656, 657, 658, 659, 660, 661, 662, 663, 664, 665, 666, 667, 668, 669, 670, 671, 672, 673, 674, 675)
+	
 	transform = transforms.Compose([
 		transforms.ToTensor(),
 		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-		
 	
-	data_set = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
+	data_set = torchvision.datasets.ImageFolder(root=args.image_path, transform=transform)
 	
 	data_loader = torch.utils.data.DataLoader(data_set, batch_size=4,
                                           shuffle=True, num_workers=2)
